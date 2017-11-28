3436a430711896235992297a01b471589900605e
japhill <jason.phillips@sciome.com>
2017-11-27 20:54:37 -0500

    worked on improvements to the range plot

--- src/com/sciome/charts/SciomeRangePlot.java
+++ src/com/sciome/charts/SciomeRangePlot.java
@@ -7,10 +7,7 @@ import com.sciome.charts.export.ChartDataExporter;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.scene.Group;
 import javafx.scene.control.Tooltip;
-import javafx.scene.layout.Region;
-import javafx.scene.shape.Line;
 
 public abstract class SciomeRangePlot extends ScrollableSciomeChart implements ChartDataExporter
 {
@@ -97,121 +94,6 @@ public abstract class SciomeRangePlot extends ScrollableSciomeChart implements C
 			return high;
 		}
 
-	}
-
-	/** Candle node used for drawing a candle */
-	private class BoxAndWhisker extends Group
-	{
-		private Line	highLowLine		= new Line();
-		private Line	topWhisker		= new Line();
-		private Line	bottomWhisker	= new Line();
-		private Region	bar				= new Region();
-		private String	seriesStyleClass;
-		private String	dataStyleClass;
-		private boolean	openAboveClose	= true;
-		private Tooltip	tooltip			= new Tooltip();
-		private int		seriesIndex		= 0;
-
-		private BoxAndWhisker(String seriesStyleClass, String dataStyleClass, int sI)
-		{
-			setAutoSizeChildren(false);
-
-			topWhisker.resizeRelocate(0.0, 0.0, 0.0, 0.0);
-			bottomWhisker.resizeRelocate(0.0, 0.0, 0.0, 0.0);
-			getChildren().addAll(highLowLine, bar, topWhisker, bottomWhisker);
-			this.seriesStyleClass = seriesStyleClass;
-			this.dataStyleClass = dataStyleClass;
-			this.seriesIndex = sI;
-			updateStyleClasses();
-
-		}
-
-		public void setSeriesAndDataStyleClasses(String seriesStyleClass, String dataStyleClass)
-		{
-			this.seriesStyleClass = seriesStyleClass;
-			this.dataStyleClass = dataStyleClass;
-
-			updateStyleClasses();
-		}
-
-		public void update(Double closeOffset, Double highOffset, Double lowOffset, Double candleWidth,
-				Double seriesOffset)
-		{
-			// openAboveClose = closeOffset > 0;
-			updateStyleClasses();
-			Double barX = 0.0;
-			if (closeOffset == null)
-			{
-				closeOffset = candleWidth;
-				barX = candleWidth / 2;
-			}
-
-			if (lowOffset == null)
-				lowOffset = closeOffset;
-			highLowLine.setStartX(lowOffset);
-
-			if (highOffset == null || highOffset == 0)
-			{
-				highOffset = closeOffset;
-				highLowLine.setEndX(highOffset - candleWidth);
-			}
-			else
-			{
-				highLowLine.setEndX(highOffset);
-			}
-
-			highLowLine.resizeRelocate(lowOffset, seriesOffset, highOffset - lowOffset, 2.0);
-			if (candleWidth == -1)
-			{
-				candleWidth = bar.prefWidth(-1);
-			}
-
-			bar.resizeRelocate(-candleWidth / 2,
-					-candleWidth / 2 + seriesOffset + highLowLine.getStrokeWidth() / 2, closeOffset,
-					candleWidth);
-
-			// bar.resizeRelocate(closeOffset,
-			// -candleWidth / 2 + seriesOffset + highLowLine.getStrokeWidth() / 2, closeOffset * -1,
-			// candleWidth);
-
-			if (highOffset != closeOffset && highOffset > 0)
-			{
-				topWhisker.setStartY(seriesOffset);
-				topWhisker.setEndY(candleWidth / 2 + seriesOffset);
-				topWhisker.resizeRelocate(highOffset, -candleWidth / 4 + seriesOffset, candleWidth / 2,
-						candleWidth / 2);
-			}
-			else
-			{
-				topWhisker.setVisible(false);
-			}
-
-			if (lowOffset != closeOffset)
-			{
-				bottomWhisker.setStartY(seriesOffset);
-				bottomWhisker.setEndY(candleWidth / 2 + seriesOffset);
-				bottomWhisker.resizeRelocate(lowOffset, -candleWidth / 4 + seriesOffset, candleWidth / 2,
-						candleWidth / 2);
-			}
-			else
-				bottomWhisker.setVisible(false);
-
-		}
-
-		private void updateStyleClasses()
-		{
-			int colorIndex = seriesIndex % 7;
-			getStyleClass().setAll("boxwhisker-bar", seriesStyleClass, dataStyleClass);
-			highLowLine.getStyleClass().setAll("boxwhisker-line", seriesStyleClass, dataStyleClass,
-					"default-color" + colorIndex);
-			bar.getStyleClass().setAll("boxwhisker-box", seriesStyleClass, dataStyleClass,
-					"default-color" + colorIndex);
-			topWhisker.getStyleClass().setAll("boxwhisker-line", seriesStyleClass, dataStyleClass,
-					"default-color" + colorIndex);
-			bottomWhisker.getStyleClass().setAll("boxwhisker-line", seriesStyleClass, dataStyleClass,
-					"default-color" + colorIndex);
-
-		}
 	}
 
 	@Override
