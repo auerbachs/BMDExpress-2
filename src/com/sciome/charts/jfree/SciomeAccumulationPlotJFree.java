ef1685f5e0550c21b9d77a55c7688c49c071028b
jasonphillips <jasonphillips@macbook-pro-3.westell.com>
2017-12-05 20:46:43 -0500

    fixed range problem with jfree accumulation chart

--- src/com/sciome/charts/jfree/SciomeAccumulationPlotJFree.java
+++ src/com/sciome/charts/jfree/SciomeAccumulationPlotJFree.java
@@ -26,24 +26,27 @@ import com.sciome.charts.model.SciomeSeries;
 import javafx.scene.Node;
 import javafx.scene.input.MouseButton;
 
-public class SciomeAccumulationPlotJFree extends SciomeAccumulationPlot{
+public class SciomeAccumulationPlotJFree extends SciomeAccumulationPlot
+{
 
-	public SciomeAccumulationPlotJFree(String title, List<ChartDataPack> chartDataPacks, String key, Double bucketsize,
-			SciomeChartListener chartListener) {
+	public SciomeAccumulationPlotJFree(String title, List<ChartDataPack> chartDataPacks, String key,
+			Double bucketsize, SciomeChartListener chartListener)
+	{
 		super(title, chartDataPacks, key, bucketsize, chartListener);
 	}
 
 	@Override
-	protected Node generateChart(String[] keys, ChartConfiguration chartConfig) {
+	protected Node generateChart(String[] keys, ChartConfiguration chartConfig)
+	{
 		String key1 = keys[0];
 		String key2 = "Accumulation";
 		Double min1 = getMinMin(key1);
 		Double min2 = 0.0;
-		Double max1 = 0.0;
+		Double max1 = getMaxMax(key1);
 		Double max2 = 0.0;
 
 		DefaultXYDataset dataset = new DefaultXYDataset();
-		
+
 		for (SciomeSeries<Number, Number> series : getSeriesData())
 		{
 			double[] domains = new double[series.getData().size()];
@@ -51,34 +54,36 @@ public class SciomeAccumulationPlotJFree extends SciomeAccumulationPlot{
 			int i = 0;
 			for (Object chartData : series.getData())
 			{
-				AccumulationData value = (AccumulationData)chartData;
+				AccumulationData value = (AccumulationData) chartData;
 				double domainvalue = value.getXValue().doubleValue();
 				double rangevalue = value.getYValue().doubleValue();
 				domains[i] = domainvalue;
 				ranges[i++] = rangevalue;
+				max2 = rangevalue;
 			}
 			dataset.addSeries(series.getName(), new double[][] { domains, ranges });
 		}
 
 		// Create chart
-		JFreeChart chart = ChartFactory.createXYLineChart(key1 + "Accumulation Plot",
-				key1, key2, dataset, PlotOrientation.VERTICAL, true, true, false);
+		JFreeChart chart = ChartFactory.createXYLineChart(key1 + "Accumulation Plot", key1, key2, dataset,
+				PlotOrientation.VERTICAL, true, true, false);
 		XYPlot plot = (XYPlot) chart.getPlot();
 		plot.setForegroundAlpha(0.1f);
 		plot.setDomainPannable(true);
 		plot.setRangePannable(true);
 		plot.setDomainAxis(SciomeNumberAxisGeneratorJFree.generateAxis(getLogXAxis().isSelected(), key1));
 		plot.setRangeAxis(SciomeNumberAxisGeneratorJFree.generateAxis(getLogYAxis().isSelected(), key2));
-		
-		//Only want to zoom in if we any values have been set in chartConfig
-		if(chartConfig != null) {
-			//Find the values for the min and max values for x and y
+
+		// Only want to zoom in if we any values have been set in chartConfig
+		if (chartConfig != null)
+		{
+			// Find the values for the min and max values for x and y
 			if (chartConfig.getMaxX() != null && chartConfig.getMinX() != null)
 			{
 				max1 = chartConfig.getMaxX();
 				min1 = chartConfig.getMinX();
 			}
-		
+
 			if (chartConfig.getMaxY() != null && chartConfig.getMinY() != null)
 			{
 				max2 = chartConfig.getMaxY();
@@ -102,31 +107,31 @@ public class SciomeAccumulationPlotJFree extends SciomeAccumulationPlot{
 				min2 = 0.0;
 				max2 = 0.1;
 			}
-			
+
 			// Set the domain and range based on these x and y values
 			NumberAxis range = (NumberAxis) plot.getRangeAxis();
 			NumberAxis domain = (NumberAxis) plot.getDomainAxis();
 			domain.setRange(min1, max1);
 			range.setRange(min2, max2);
 		}
-		
+
 		XYLineAndShapeRenderer renderer = ((XYLineAndShapeRenderer) plot.getRenderer());
 		renderer.setDefaultShapesVisible(true);
 		renderer.setDrawOutlines(true);
 		renderer.setUseFillPaint(true);
-        renderer.setDefaultFillPaint(Color.white);
-        renderer.setSeriesStroke(0, new BasicStroke(3.0f));
-        renderer.setSeriesOutlineStroke(0, new BasicStroke(2.0f));
-        renderer.setSeriesShape(0, new Ellipse2D.Double(-5.0, -5.0, 10.0, 10.0));
+		renderer.setDefaultFillPaint(Color.white);
+		renderer.setSeriesStroke(0, new BasicStroke(3.0f));
+		renderer.setSeriesOutlineStroke(0, new BasicStroke(2.0f));
+		renderer.setSeriesShape(0, new Ellipse2D.Double(-5.0, -5.0, 10.0, 10.0));
 		renderer.setSeriesPaint(0, new Color(0.0f, 0.0f, .82f, .5f));
-		
-		//Set tooltip string
-		XYToolTipGenerator tooltipGenerator = new XYToolTipGenerator()
-		{
+
+		// Set tooltip string
+		XYToolTipGenerator tooltipGenerator = new XYToolTipGenerator() {
 			@Override
-			public String generateToolTip(XYDataset dataset, int series, int item) {
-				AccumulationData data = (AccumulationData)getSeriesData().get(series).getData().get(item);
-				List<Object> objects = (List<Object>)(data.getExtraValue());
+			public String generateToolTip(XYDataset dataset, int series, int item)
+			{
+				AccumulationData data = (AccumulationData) getSeriesData().get(series).getData().get(item);
+				List<Object> objects = (List<Object>) (data.getExtraValue());
 				return String.valueOf(joinObjects(objects, data.getYValue().doubleValue(),
 						data.getValuesList(), key1, MAX_TO_POPUP));
 			}
@@ -142,20 +147,24 @@ public class SciomeAccumulationPlotJFree extends SciomeAccumulationPlot{
 		chartView.addChartMouseListener(new ChartMouseListenerFX() {
 
 			@Override
-			public void chartMouseClicked(ChartMouseEventFX e) {
-				if(e.getEntity() != null && e.getEntity().getToolTipText() != null //Check to see if an entity was clicked
-						&& e.getTrigger().getButton().equals(MouseButton.PRIMARY)) //Check to see if it was the left mouse button clicked
-				showObjectText(e.getEntity().getToolTipText());
+			public void chartMouseClicked(ChartMouseEventFX e)
+			{
+				if (e.getEntity() != null && e.getEntity().getToolTipText() != null // Check to see if an
+																					// entity was clicked
+						&& e.getTrigger().getButton().equals(MouseButton.PRIMARY)) // Check to see if it was
+																					// the left mouse button
+																					// clicked
+					showObjectText(e.getEntity().getToolTipText());
 			}
 
 			@Override
-			public void chartMouseMoved(ChartMouseEventFX e) {
-				//ignore for now
+			public void chartMouseMoved(ChartMouseEventFX e)
+			{
+				// ignore for now
 			}
 		});
 
 		return chartView;
 	}
 
-	
 }
