a107d58b028bca497e04da286f9c4e8042f40d91
jasonphillips <jasonphillips@macbook-pro-3.westell.com>
2017-12-05 20:41:05 -0500

    fixed custom log axis for curve plot view.

--- src/com/sciome/charts/jfree/SciomeNumberAxisGeneratorJFree.java
+++ src/com/sciome/charts/jfree/SciomeNumberAxisGeneratorJFree.java
@@ -4,16 +4,20 @@ import org.jfree.chart.axis.LogarithmicAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.ValueAxis;
 
-public class SciomeNumberAxisGeneratorJFree {
+public class SciomeNumberAxisGeneratorJFree
+{
 
 	public static ValueAxis generateAxis(boolean isLog, String label)
 	{
 		ValueAxis axis;
 		if (isLog)
+		{
 			axis = new LogarithmicAxis(label);
+			((LogarithmicAxis) axis).setStrictValuesFlag(false);
+		}
 		else
 			axis = new NumberAxis(label);
-		
+
 		return axis;
 	}
 }
