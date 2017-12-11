f701c6fd9f1f3b03a8bd3cc3502d18b898f34981
jasonphillips <jasonphillips@10.0.0.30>
2017-12-11 00:09:58 -0500

    fixed bug while loading empty series in histogramjfree

--- src/com/sciome/charts/jfree/SciomeHistogramJFree.java
+++ src/com/sciome/charts/jfree/SciomeHistogramJFree.java
@@ -4,12 +4,9 @@ import java.awt.Color;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.fx.interaction.ChartMouseEventFX;
 import org.jfree.chart.fx.interaction.ChartMouseListenerFX;
 import org.jfree.chart.labels.XYToolTipGenerator;
@@ -29,33 +26,24 @@ import com.sciome.charts.export.ChartDataExporter;
 import com.sciome.charts.model.SciomeData;
 import com.sciome.charts.model.SciomeSeries;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.util.Callback;
 
-public class SciomeHistogramJFree extends SciomeHistogram implements ChartDataExporter {
+public class SciomeHistogramJFree extends SciomeHistogram implements ChartDataExporter
+{
 
-	public SciomeHistogramJFree(String title, List<ChartDataPack> chartDataPacks, String key, Double bucketsize,
-			SciomeChartListener chartListener) {
+	public SciomeHistogramJFree(String title, List<ChartDataPack> chartDataPacks, String key,
+			Double bucketsize, SciomeChartListener chartListener)
+	{
 		super(title, chartDataPacks, key, bucketsize, chartListener);
 	}
 
 	@Override
-	protected Node generateChart(String[] keys, ChartConfiguration chartConfig) {
+	protected Node generateChart(String[] keys, ChartConfiguration chartConfig)
+	{
 		String key = keys[0];
 
-		//Create dataset
+		// Create dataset
 		HistogramDataset dataset = new HistogramDataset();
 		for (ChartDataPack chartDataPack : getChartDataPacks())
 		{
@@ -68,70 +56,78 @@ public class SciomeHistogramJFree extends SciomeHistogram implements ChartDataEx
 					continue;
 				ranges[i++] = dataPoint.doubleValue();
 			}
-			dataset.addSeries(chartDataPack.getName(), ranges, (int)bucketsize.doubleValue());
+			if (ranges.length > 0)
+				dataset.addSeries(chartDataPack.getName(), ranges, (int) bucketsize.doubleValue());
 		}
-		
+
 		// Create chart
-		JFreeChart chart = ChartFactory.createHistogram(key + " Histogram", key, "Count", dataset, PlotOrientation.VERTICAL, true, true, false);
-		
-		//Set plot parameters
+		JFreeChart chart = ChartFactory.createHistogram(key + " Histogram", key, "Count", dataset,
+				PlotOrientation.VERTICAL, true, true, false);
+
+		// Set plot parameters
 		XYPlot plot = (XYPlot) chart.getPlot();
 		plot.setForegroundAlpha(0.1f);
 		plot.setDomainPannable(true);
 		plot.setRangePannable(true);
 		plot.setRangeAxis(SciomeNumberAxisGeneratorJFree.generateAxis(getLogYAxis().isSelected(), key));
-		
-		//Set renderer parameters
+
+		// Set renderer parameters
 		XYBarRenderer renderer = ((XYBarRenderer) plot.getRenderer());
-		//Set tooltip string
-		XYToolTipGenerator tooltipGenerator = new XYToolTipGenerator()
-		{
+		// Set tooltip string
+		XYToolTipGenerator tooltipGenerator = new XYToolTipGenerator() {
 			@Override
-			public String generateToolTip(XYDataset dataset, int series, int item) {
-				List<Object> objects = (List<Object>)(getSeriesData().get(series).getData().get(item).getExtraValue());
+			public String generateToolTip(XYDataset dataset, int series, int item)
+			{
+				List<Object> objects = (List<Object>) (getSeriesData().get(series).getData().get(item)
+						.getExtraValue());
 				return String.valueOf(joinAllObjects(objects));
 			}
 		};
 		renderer.setSeriesFillPaint(0, Color.white);
 		renderer.setDefaultOutlinePaint(Color.black);
 		renderer.setDefaultToolTipGenerator(tooltipGenerator);
-        renderer.setBarPainter(new StandardXYBarPainter());
-        renderer.setShadowVisible(false);
+		renderer.setBarPainter(new StandardXYBarPainter());
+		renderer.setShadowVisible(false);
 		plot.setBackgroundPaint(Color.white);
 		chart.getPlot().setForegroundAlpha(0.5f);
-		
+
 		// Create Custom ChartViewer to deal with zooming
 		SciomeChartViewer chartView = new SciomeChartViewer(chart);
-		
-		//Add plot point clicking interaction
+
+		// Add plot point clicking interaction
 		chartView.addChartMouseListener(new ChartMouseListenerFX() {
 
 			@Override
-			public void chartMouseClicked(ChartMouseEventFX e) {
-				if(e.getEntity() != null && e.getEntity().getToolTipText() != null //Check to see if an entity was clicked
-						&& e.getTrigger().getButton().equals(MouseButton.PRIMARY)) //Check to see if it was the left mouse button clicked
+			public void chartMouseClicked(ChartMouseEventFX e)
+			{
+				if (e.getEntity() != null && e.getEntity().getToolTipText() != null // Check to see if an
+																					// entity was clicked
+						&& e.getTrigger().getButton().equals(MouseButton.PRIMARY)) // Check to see if it was
+																					// the left mouse button
+																					// clicked
 					showObjectText(e.getEntity().getToolTipText());
 			}
 
 			@Override
-			public void chartMouseMoved(ChartMouseEventFX e) {
-				//ignore for now
+			public void chartMouseMoved(ChartMouseEventFX e)
+			{
+				// ignore for now
 			}
 		});
-		
+
 		return chartView;
 	}
-	
-	//This method is overridden since jfree has it's own implementation for histograms that generates
+
+	// This method is overridden since jfree has it's own implementation for histograms that generates
 	// buckets by itself which we need to map our objects to
 	@Override
 	protected void convertChartDataPacksToSciomeSeries(String[] keys, List<ChartDataPack> chartPacks)
 	{
 		String key = keys[0];
 		// Now put the data in a bucket
-		
-		//We currently need to generate the jfree dataset to create the SciomeSeries for the purpose of
-		//being able to map each item with the extravalue object for the tooltip
+
+		// We currently need to generate the jfree dataset to create the SciomeSeries for the purpose of
+		// being able to map each item with the extravalue object for the tooltip
 		HistogramDataset dataset = new HistogramDataset();
 		for (ChartDataPack chartDataPack : getChartDataPacks())
 		{
@@ -144,33 +140,41 @@ public class SciomeHistogramJFree extends SciomeHistogram implements ChartDataEx
 					continue;
 				values[i++] = dataPoint.doubleValue();
 			}
-			dataset.addSeries(key, values, (int)bucketsize.doubleValue());
+			if (values.length > 0)
+				dataset.addSeries(key, values, (int) bucketsize.doubleValue());
 		}
 
 		List<SciomeSeries<String, Number>> seriesData = new ArrayList<>();
-		
-		for(int i = 0; i < dataset.getSeriesCount(); i++) {
+
+		for (int i = 0; i < dataset.getSeriesCount(); i++)
+		{
 			List<List<Object>> bucketObjects = new ArrayList<>();
-			if(bucketsize != null) {
+			if (bucketsize != null)
+			{
 				for (int j = 0; j < bucketsize.intValue(); j++)
 					bucketObjects.add(new ArrayList<>());
 			}
-			//Loop through all the data points and put them into the appropriate bucket
-			for(ChartData data : getChartDataPacks().get(i).getChartData()) {
-				Double dataPoint = (Double)(data.getDataPoints().get(key));
-				if(dataPoint != null) {
-					for(int j = 0; j < bucketsize.intValue(); j++) {
-						if(dataPoint > dataset.getStartX(i, j).doubleValue() && dataPoint < dataset.getEndX(i, j).doubleValue()) {
+			// Loop through all the data points and put them into the appropriate bucket
+			for (ChartData data : getChartDataPacks().get(i).getChartData())
+			{
+				Double dataPoint = (Double) (data.getDataPoints().get(key));
+				if (dataPoint != null)
+				{
+					for (int j = 0; j < bucketsize.intValue(); j++)
+					{
+						if (dataPoint > dataset.getStartX(i, j).doubleValue()
+								&& dataPoint < dataset.getEndX(i, j).doubleValue())
+						{
 							bucketObjects.get(j).add(data.getCharttableObject());
 						}
 					}
 				}
 			}
-			
+
 			SciomeSeries<String, Number> series = new SciomeSeries<>();
 			series.setName(getChartDataPacks().get(i).getName());
 
-			//Create the SciomeData object for each
+			// Create the SciomeData object for each
 			DecimalFormat df = new DecimalFormat("#.###");
 			for (int j = 0; j < bucketsize.intValue(); j++)
 			{
@@ -178,7 +182,8 @@ public class SciomeHistogramJFree extends SciomeHistogram implements ChartDataEx
 						dataset.getY(i, j), bucketObjects.get(j));
 				series.getData().add(data);
 			}
-			seriesData.add(series);
+			if (series.getData().size() > 0)
+				seriesData.add(series);
 		}
 		setSeriesData(seriesData);
 	}
