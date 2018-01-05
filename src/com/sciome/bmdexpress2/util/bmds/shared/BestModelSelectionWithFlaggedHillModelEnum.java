0717fe0aa4be16a80dfa934c8255c6856422de74
japhill <jason.phillips@sciome.com>
2018-01-05 14:08:28 -0500

    removed "(default)" from inlude flagged hill

--- src/com/sciome/bmdexpress2/util/bmds/shared/BestModelSelectionWithFlaggedHillModelEnum.java
+++ src/com/sciome/bmdexpress2/util/bmds/shared/BestModelSelectionWithFlaggedHillModelEnum.java
@@ -2,7 +2,7 @@ package com.sciome.bmdexpress2.util.bmds.shared;
 
 public enum BestModelSelectionWithFlaggedHillModelEnum
 {
-	INCLUDE_FLAGGED_HILL("Include Flagged Hill (default)"), EXCLUDE_FLAGGED_HILL_FROM_BEST(
+	INCLUDE_FLAGGED_HILL("Include Flagged Hill"), EXCLUDE_FLAGGED_HILL_FROM_BEST(
 			"Exclude Flagged Hill from Best Models"), EXCLUDE_ALL_HILL_FROM_BEST(
 					"Exclude All Hill from Best Models"), MODIFY_BMD_IF_FLAGGED_HILL_BEST(
 							"Modify BMD if Flagged Hill as Best Model"), SELECT_NEXT_BEST_PVALUE_GREATER_OO5(
