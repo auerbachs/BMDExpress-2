ca720d0f66f150cd08103b5c0472654139e9ceac
shyam.patel <shyam.patel@LAP-DELL-SP.sciome.work>
2018-07-30 17:32:45 -0400

    Added the ability to change the series colors of jfree charts

--- src/com/sciome/charts/jfree/editor/DefaultChartEditor.java
+++ src/com/sciome/charts/jfree/editor/DefaultChartEditor.java
@@ -44,24 +44,35 @@
 package com.sciome.charts.jfree.editor;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.Paint;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JColorChooser;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.PolarPlot;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.title.Title;
 import org.jfree.chart.util.ResourceBundleWrapper;
 import org.jfree.layout.LCBLayout;
@@ -72,206 +83,307 @@ import org.jfree.ui.PaintSample;
  * legend and plot).
  */
 class DefaultChartEditor extends JPanel implements ActionListener, ChartEditor {
+	private static final String SERIES_COLOR = "Series Color";
 
-    /** A panel for displaying/editing the properties of the title. */
-    private DefaultTitleEditor titleEditor;
+	/** A panel for displaying/editing the properties of the title. */
+	private DefaultTitleEditor titleEditor;
 
-    /** A panel for displaying/editing the properties of the plot. */
-    private DefaultPlotEditor plotEditor;
+	/** A panel for displaying/editing the properties of the plot. */
+	private DefaultPlotEditor plotEditor;
 
-    /**
-     * A checkbox indicating whether or not the chart is drawn with
-     * anti-aliasing.
-     */
-    private JCheckBox antialias;
+	/**
+	 * A checkbox indicating whether or not the chart is drawn with anti-aliasing.
+	 */
+	private JCheckBox antialias;
 
-    /** The chart background color. */
-    private PaintSample background;
+	/** The chart background color. */
+	private PaintSample background;
 
-    /** The resourceBundle for the localization. */
-    protected static ResourceBundle localizationResources
-            = ResourceBundleWrapper.getBundle(
-                    "org.jfree.chart.editor.LocalizationBundle");
+	/** The current series color */
+	private ArrayList<Paint> seriesColor;
+	
+	/** The color panel used to display the current series colors */
+	private JPanel colorPanel;
 
-    /**
-     * Standard constructor - the property panel is made up of a number of
-     * sub-panels that are displayed in the tabbed pane.
-     *
-     * @param chart  the chart, whichs properties should be changed.
-     */
-    public DefaultChartEditor(JFreeChart chart) {
-        setLayout(new BorderLayout());
+	/** The resourceBundle for the localization. */
+	protected static ResourceBundle localizationResources = ResourceBundleWrapper
+			.getBundle("org.jfree.chart.editor.LocalizationBundle");
 
-        JPanel other = new JPanel(new BorderLayout());
-        other.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+	/**
+	 * Standard constructor - the property panel is made up of a number of
+	 * sub-panels that are displayed in the tabbed pane.
+	 *
+	 * @param chart
+	 *            the chart, whichs properties should be changed.
+	 */
+	public DefaultChartEditor(JFreeChart chart) {
+		setLayout(new BorderLayout());
 
-        JPanel general = new JPanel(new BorderLayout());
-        general.setBorder(BorderFactory.createTitledBorder(
-            BorderFactory.createEtchedBorder(),
-            localizationResources.getString("General")));
+		JPanel other = new JPanel(new BorderLayout());
+		other.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
 
-        JPanel interior = new JPanel(new LCBLayout(6));
-        interior.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+		JPanel general = new JPanel(new BorderLayout());
+		general.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
+				localizationResources.getString("General")));
 
-        this.antialias = new JCheckBox(localizationResources.getString(
-                "Draw_anti-aliased"));
-        this.antialias.setSelected(chart.getAntiAlias());
-        interior.add(this.antialias);
-        interior.add(new JLabel(""));
-        interior.add(new JLabel(""));
-        interior.add(new JLabel(localizationResources.getString(
-                "Background_paint")));
-        this.background = new PaintSample(chart.getBackgroundPaint());
-        interior.add(this.background);
-        JButton button = new JButton(localizationResources.getString(
-                "Select..."));
-        button.setActionCommand("BackgroundPaint");
-        button.addActionListener(this);
-        interior.add(button);
+		JPanel interior = new JPanel(new LCBLayout(6));
+		interior.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
 
-        interior.add(new JLabel(localizationResources.getString(
-                "Series_Paint")));
-        JTextField info = new JTextField(localizationResources.getString(
-                "No_editor_implemented"));
-        info.setEnabled(false);
-        interior.add(info);
-        button = new JButton(localizationResources.getString("Edit..."));
-        button.setEnabled(false);
-        interior.add(button);
+		this.antialias = new JCheckBox(localizationResources.getString("Draw_anti-aliased"));
+		this.antialias.setSelected(chart.getAntiAlias());
+		interior.add(this.antialias);
+		interior.add(new JLabel(""));
+		interior.add(new JLabel(""));
+		interior.add(new JLabel(localizationResources.getString("Background_paint")));
+		this.background = new PaintSample(chart.getBackgroundPaint());
+		interior.add(this.background);
+		JButton button = new JButton(localizationResources.getString("Select..."));
+		button.setActionCommand("BackgroundPaint");
+		button.addActionListener(this);
+		interior.add(button);
 
-        interior.add(new JLabel(localizationResources.getString(
-                "Series_Stroke")));
-        info = new JTextField(localizationResources.getString(
-                "No_editor_implemented"));
-        info.setEnabled(false);
-        interior.add(info);
-        button = new JButton(localizationResources.getString("Edit..."));
-        button.setEnabled(false);
-        interior.add(button);
+		interior.add(new JLabel(localizationResources.getString("Series_Paint")));
+		button = new JButton(localizationResources.getString("Edit..."));
 
-        interior.add(new JLabel(localizationResources.getString(
-                "Series_Outline_Paint")));
-        info = new JTextField(localizationResources.getString(
-                "No_editor_implemented"));
-        info.setEnabled(false);
-        interior.add(info);
-        button = new JButton(localizationResources.getString("Edit..."));
-        button.setEnabled(false);
-        interior.add(button);
+		JTextField info = new JTextField(localizationResources.getString("No_editor_implemented"));
+		Plot plot = chart.getPlot();
+		if (plot instanceof XYPlot) {
+			seriesColor = new ArrayList<Paint>();
+			colorPanel = new JPanel();
+			for (int i = 0; i < ((XYPlot) plot).getSeriesCount(); i++) {
+				seriesColor.add(((XYPlot) plot).getRenderer().getSeriesPaint(i));
+				colorPanel.add(new PaintSample(seriesColor.get(i)));
+			}
+			interior.add(colorPanel);
+			button.setActionCommand(SERIES_COLOR);
+			button.addActionListener(this);
+		} else if (plot instanceof CategoryPlot) {
+			seriesColor = new ArrayList<Paint>();
+			colorPanel = new JPanel();
+			for (int i = 0; i < ((CategoryPlot) plot).getDataset().getRowCount(); i++) {
+				seriesColor.add(((CategoryPlot) plot).getRenderer().getSeriesPaint(i));
+				colorPanel.add(new PaintSample(seriesColor.get(i)));
+			}
+			interior.add(colorPanel);
+			button.setActionCommand(SERIES_COLOR);
+			button.addActionListener(this);
+		} else {
+			info.setEnabled(false);
+			button.setEnabled(false);
+			interior.add(info);
+		}
+		interior.add(button);
 
-        interior.add(new JLabel(localizationResources.getString(
-                "Series_Outline_Stroke")));
-        info = new JTextField(localizationResources.getString(
-                "No_editor_implemented"));
-        info.setEnabled(false);
-        interior.add(info);
-        button = new JButton(localizationResources.getString("Edit..."));
-        button.setEnabled(false);
-        interior.add(button);
+		interior.add(new JLabel(localizationResources.getString("Series_Stroke")));
+		info = new JTextField(localizationResources.getString("No_editor_implemented"));
+		info.setEnabled(false);
+		interior.add(info);
+		button = new JButton(localizationResources.getString("Edit..."));
+		button.setEnabled(false);
+		interior.add(button);
 
-        general.add(interior, BorderLayout.NORTH);
-        other.add(general, BorderLayout.NORTH);
+		interior.add(new JLabel(localizationResources.getString("Series_Outline_Paint")));
+		info = new JTextField(localizationResources.getString("No_editor_implemented"));
+		info.setEnabled(false);
+		interior.add(info);
+		button = new JButton(localizationResources.getString("Edit..."));
+		button.setEnabled(false);
+		interior.add(button);
 
-        JPanel parts = new JPanel(new BorderLayout());
+		interior.add(new JLabel(localizationResources.getString("Series_Outline_Stroke")));
+		info = new JTextField(localizationResources.getString("No_editor_implemented"));
+		info.setEnabled(false);
+		interior.add(info);
+		button = new JButton(localizationResources.getString("Edit..."));
+		button.setEnabled(false);
+		interior.add(button);
 
-        Title title = chart.getTitle();
-        Plot plot = chart.getPlot();
+		general.add(interior, BorderLayout.NORTH);
+		other.add(general, BorderLayout.NORTH);
 
-        JTabbedPane tabs = new JTabbedPane();
+		JPanel parts = new JPanel(new BorderLayout());
 
-        this.titleEditor = new DefaultTitleEditor(title);
-        this.titleEditor.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-        tabs.addTab(localizationResources.getString("Title"), this.titleEditor);
+		Title title = chart.getTitle();
 
-        if (plot instanceof PolarPlot) {
-            this.plotEditor = new DefaultPolarPlotEditor((PolarPlot) plot);
-        }
-        else {
-            this.plotEditor = new DefaultPlotEditor(plot);
-        }
-        this.plotEditor.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-        tabs.addTab(localizationResources.getString("Plot"), this.plotEditor);
+		JTabbedPane tabs = new JTabbedPane();
 
-        tabs.add(localizationResources.getString("Other"), other);
-        parts.add(tabs, BorderLayout.NORTH);
-        add(parts);
-    }
+		this.titleEditor = new DefaultTitleEditor(title);
+		this.titleEditor.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		tabs.addTab(localizationResources.getString("Title"), this.titleEditor);
 
-    /**
-     * Returns a reference to the title editor.
-     *
-     * @return A panel for editing the title.
-     */
-    public DefaultTitleEditor getTitleEditor() {
-      return this.titleEditor;
-    }
+		if (plot instanceof PolarPlot) {
+			this.plotEditor = new DefaultPolarPlotEditor((PolarPlot) plot);
+		} else {
+			this.plotEditor = new DefaultPlotEditor(plot);
+		}
+		this.plotEditor.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		tabs.addTab(localizationResources.getString("Plot"), this.plotEditor);
 
-    /**
-     * Returns a reference to the plot property sub-panel.
-     *
-     * @return A panel for editing the plot properties.
-     */
-    public DefaultPlotEditor getPlotEditor() {
-        return this.plotEditor;
-    }
+		tabs.add(localizationResources.getString("Other"), other);
+		parts.add(tabs, BorderLayout.NORTH);
+		add(parts);
+	}
 
-    /**
-     * Returns the current setting of the anti-alias flag.
-     *
-     * @return <code>true</code> if anti-aliasing is enabled.
-     */
-    public boolean getAntiAlias() {
-        return this.antialias.isSelected();
-    }
+	/**
+	 * Returns a reference to the title editor.
+	 *
+	 * @return A panel for editing the title.
+	 */
+	public DefaultTitleEditor getTitleEditor() {
+		return this.titleEditor;
+	}
 
-    /**
-     * Returns the current background paint.
-     *
-     * @return The current background paint.
-     */
-    public Paint getBackgroundPaint() {
-        return this.background.getPaint();
-    }
+	/**
+	 * Returns a reference to the plot property sub-panel.
+	 *
+	 * @return A panel for editing the plot properties.
+	 */
+	public DefaultPlotEditor getPlotEditor() {
+		return this.plotEditor;
+	}
 
-    /**
-     * Handles user interactions with the panel.
-     *
-     * @param event  a BackgroundPaint action.
-     */
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        String command = event.getActionCommand();
-        if (command.equals("BackgroundPaint")) {
-            attemptModifyBackgroundPaint();
-        }
-    }
+	/**
+	 * Returns the current setting of the anti-alias flag.
+	 *
+	 * @return <code>true</code> if anti-aliasing is enabled.
+	 */
+	public boolean getAntiAlias() {
+		return this.antialias.isSelected();
+	}
 
-    /**
-     * Allows the user the opportunity to select a new background paint.  Uses
-     * JColorChooser, so we are only allowing a subset of all Paint objects to
-     * be selected (fix later).
-     */
-    private void attemptModifyBackgroundPaint() {
-        Color c;
-        c = JColorChooser.showDialog(new FrontDialog(this), localizationResources.getString(
-                "Background_Color"), Color.blue);
-        if (c != null) {
-            this.background.setPaint(c);
-        }
-    }
+	/**
+	 * Returns the current background paint.
+	 *
+	 * @return The current background paint.
+	 */
+	public Paint getBackgroundPaint() {
+		return this.background.getPaint();
+	}
 
-    /**
-     * Updates the properties of a chart to match the properties defined on the
-     * panel.
-     *
-     * @param chart  the chart.
-     */
-    @Override
-    public void updateChart(JFreeChart chart) {
-        this.titleEditor.setTitleProperties(chart);
-        this.plotEditor.updatePlotProperties(chart.getPlot());
-        chart.setAntiAlias(getAntiAlias());
-        chart.setBackgroundPaint(getBackgroundPaint());
-    }
+	/**
+	 * Handles user interactions with the panel.
+	 *
+	 * @param event
+	 *            a BackgroundPaint action.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		String command = event.getActionCommand();
+		if (command.equals("BackgroundPaint")) {
+			attemptModifyBackgroundPaint();
+		} else if (command.equals(SERIES_COLOR)) {
+			attemptModifySeriesPaint();
+		}
+	}
 
+	/**
+	 * Allows the user the opportunity to select a new background paint. Uses
+	 * JColorChooser, so we are only allowing a subset of all Paint objects to be
+	 * selected (fix later).
+	 */
+	private void attemptModifyBackgroundPaint() {
+		Color c;
+		c = JColorChooser.showDialog(new FrontDialog(this), localizationResources.getString("Background_Color"),
+				Color.blue);
+		if (c != null) {
+			this.background.setPaint(c);
+		}
+	}
+
+	/**
+	 * Allows the user the opportunity to select a new set of series paint. Uses
+	 * JColorChooser, so we are only allowing a subset of all Paint objects to be
+	 * selected (fix later).
+	 */
+	private void attemptModifySeriesPaint() {
+		JDialog dialog = new FrontDialog(this);
+		JPanel panel = new JPanel();
+		GridLayout layout = new GridLayout(seriesColor.size() + 1, 2);
+		panel.setLayout(layout);
+		
+		ArrayList<Paint> tempSeriesColor = new ArrayList<Paint>();
+		for(int i = 0; i < seriesColor.size(); i++) {
+			tempSeriesColor.add(seriesColor.get(i));
+		}
+		
+		final Component comp = this;
+		//Create action listeners for all the buttons
+		ActionListener listener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(e.getActionCommand().equals("Ok")) {
+					seriesColor = tempSeriesColor;
+					setColorPanel();
+					dialog.setVisible(false);
+				} else if(e.getActionCommand().equals("Cancel")) {
+					dialog.setVisible(false);
+				} else {
+					//Open up the color chooser
+					Color c;
+					Integer series = Integer.parseInt(e.getActionCommand());
+					c = JColorChooser.showDialog(new FrontDialog(comp), "Series " + series + " Color",
+							Color.blue);
+					if (c != null) {
+						((Button)e.getSource()).setBackground(c);
+						tempSeriesColor.set(series, c);
+					}
+				}
+			}
+		};
+		
+		for(int i = 0; i < seriesColor.size(); i++) {
+			panel.add(new Label("Series " + (i + 1)));
+			Button btn = new Button("");
+			btn.setBackground((Color)seriesColor.get(i));
+			btn.setActionCommand("" + i);
+			btn.addActionListener(listener);
+			panel.add(btn);
+		}
+		Button ok = new Button("Ok");
+		ok.setActionCommand("Ok");
+		ok.addActionListener(listener);
+		Button cancel = new Button("Cancel");
+		cancel.setActionCommand("Cancel");
+		cancel.addActionListener(listener);
+		panel.add(ok);
+		panel.add(cancel);
+		
+		JScrollPane scroll = new JScrollPane(panel);
+		dialog.add(scroll);
+		dialog.setSize(200, 300);
+		dialog.setVisible(true);
+	}
+
+	/**
+	 * Updates the properties of a chart to match the properties defined on the
+	 * panel.
+	 *
+	 * @param chart
+	 *            the chart.
+	 */
+	@Override
+	public void updateChart(JFreeChart chart) {
+		this.titleEditor.setTitleProperties(chart);
+		this.plotEditor.updatePlotProperties(chart.getPlot());
+		chart.setAntiAlias(getAntiAlias());
+		chart.setBackgroundPaint(getBackgroundPaint());
+		Plot plot = chart.getPlot();
+		if(plot instanceof XYPlot) {
+			for(int i = 0; i < seriesColor.size(); i++)
+				((XYPlot) plot).getRenderer().setSeriesPaint(i, seriesColor.get(i));
+		} else if(plot instanceof CategoryPlot) {
+			for(int i = 0; i < seriesColor.size(); i++)
+				((CategoryPlot) plot).getRenderer().setSeriesPaint(i, seriesColor.get(i));
+		} 
+	}
+	
+	/**
+	 * Reset the color panel with the new series colors
+	 */
+	private void setColorPanel() {
+		colorPanel.removeAll();
+		for(int i = 0; i < seriesColor.size(); i++) 
+			colorPanel.add(new PaintSample(seriesColor.get(i)));
+		colorPanel.revalidate();
+	}
 }
