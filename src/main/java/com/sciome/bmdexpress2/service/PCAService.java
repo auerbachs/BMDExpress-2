1fdc2f99f5ccb40ee460456a867e68fe5f7fe3b4
Jason Phillips <jason.phillips@sciome.com>
2020-12-18 16:06:49 -0500

    update pca service with latest math lib

--- src/main/java/com/sciome/bmdexpress2/service/PCAService.java
+++ src/main/java/com/sciome/bmdexpress2/service/PCAService.java
@@ -12,44 +12,53 @@ import com.sciome.bmdexpress2.mvp.model.pca.PCAResults;
 import com.sciome.bmdexpress2.mvp.model.probe.ProbeResponse;
 import com.sciome.bmdexpress2.mvp.model.probe.Treatment;
 import com.sciome.bmdexpress2.serviceInterface.IPCAService;
-import com.sciome.commons.math.PCA.PCA;
-import com.sciome.commons.math.PCA.PCA.CovarianceType;
+import com.sciome.commons.math.PCA.old.PCA;
+import com.sciome.commons.math.PCA.old.PCA.CovarianceType;
 
-public class PCAService implements IPCAService {
+public class PCAService implements IPCAService
+{
 
 	@Override
-	public PCAResults calculatePCA(DoseResponseExperiment doseResponseExperiment) {
+	public PCAResults calculatePCA(DoseResponseExperiment doseResponseExperiment)
+	{
 		List<ProbeResponse> responses = doseResponseExperiment.getProbeResponses();
 		List<Treatment> treatments = doseResponseExperiment.getTreatments();
 		double[][] numericMatrix = new double[responses.size()][responses.get(0).getResponses().size()];
 		double[] doseVector = new double[treatments.size()];
-		
-		//Fill numeric matrix
-		for(int i = 0; i < numericMatrix.length; i++) {
-			for(int j = 0; j < numericMatrix[i].length; j++) {
+
+		// Fill numeric matrix
+		for (int i = 0; i < numericMatrix.length; i++)
+		{
+			for (int j = 0; j < numericMatrix[i].length; j++)
+			{
 				numericMatrix[i][j] = responses.get(i).getResponses().get(j);
 			}
 		}
-		//Fill doseVector
-		for(int i = 0; i < doseVector.length; i++) {
+		// Fill doseVector
+		for (int i = 0; i < doseVector.length; i++)
+		{
 			doseVector[i] = treatments.get(i).getDose();
 		}
-		
-		PCA pca = new PCA(MatrixUtils.createRealMatrix(numericMatrix).transpose(), CovarianceType.COVARIANCE, 4);
+
+		PCA pca = new PCA(MatrixUtils.createRealMatrix(numericMatrix).transpose(), CovarianceType.COVARIANCE,
+				4);
 		PCAResults pcaResults = new PCAResults();
 		List<PCAResult> pcaResultList = new ArrayList<PCAResult>();
 		RealMatrix transformedData = pca.getTransformedData();
-		for(int i = 0; i < transformedData.getRowDimension(); i++) {
+		for (int i = 0; i < transformedData.getRowDimension(); i++)
+		{
 			PCAResult singleRow = new PCAResult();
-			singleRow.setDosage(doseVector[i] + " - " + doseResponseExperiment.getTreatments().get(i).getName());
+			singleRow.setDosage(
+					doseVector[i] + " - " + doseResponseExperiment.getTreatments().get(i).getName());
 			List<Float> floatList = new ArrayList<Float>();
-			for(int j = 0; j < transformedData.getColumnDimension(); j++) {
+			for (int j = 0; j < transformedData.getColumnDimension(); j++)
+			{
 				floatList.add((float) transformedData.getEntry(i, j));
 			}
 			singleRow.setPrincipleComponents(floatList);
 			pcaResultList.add(singleRow);
 		}
-		
+
 		pcaResults.setName(doseResponseExperiment.getName() + "_PCA");
 		pcaResults.setPcaResults(pcaResultList);
 		return pcaResults;
